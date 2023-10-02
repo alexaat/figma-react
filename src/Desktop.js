a9be426fc798937b526e97a712ba917a4cae2b98
avidaseu <alexaat@yahoo.co.uk>
2023-10-02 18:47:56 +0100

    Resize

--- src/Desktop.js
+++ src/Desktop.js
@@ -4,6 +4,7 @@ import logo from './images/logo.jpeg';
 import phones from './images/phones.png';
 import magicLink from './images/magic_link.png';
 import CheckCircleRoundedIcon from '@mui/icons-material/CheckCircleRounded';
+import useWindowSize from "./useWindowSize.js";
 
 import {useState} from 'react';
 
@@ -14,39 +15,56 @@ const Desktop = () => {
     const [open, setOpen] = useState(false);
 
     const handleSignUpClick = e => {
-      console.log('')
       setOpen(true);
     }
-
+    const size = useWindowSize();  
     return (
-
       <>
-      <Stack direction='column'>
+
+  {size.width > 768 &&
+      <Stack direction='column' className='main'>
+        
+
+
           <AppBar position="static"  elevation={0} style={{ background: '#FFFFFF'}}>
-            <Toolbar sx={{ justifyContent: "space-between"}}>
-               
-                  <Box component="img" alt="logo" src={logo} sx={{ height: '96px'}}></Box>
-                  <Stack direction="row">
-                    <MenuButton title="Join Firesides"/>
-                    <MenuButton title="Partner with us"/>
-                    <MenuButton title="Team"/>
-                    <MenuButton title="Blog"/>
-                    <Box sx={{borderLeft: 4, borderColor: '#eee', pl: 2}}>
-                      <MenuButton title="Log In" color='white'/>
-                      <MenuButton title="Create Account" color='black'/>
-                    </Box>
-                  </Stack>
-            </Toolbar>
-          </AppBar>
+          <Toolbar sx={{ justifyContent: "space-between"}} >
+            
+                <Box component="img" alt="logo" src={logo} sx={{ height: '96px'}}></Box>
+                <Stack direction="row">
+                  <MenuButton title="Join Firesides"/>
+                  <MenuButton title="Partner with us"/>
+                  <MenuButton title="Team"/>
+                  <MenuButton title="Blog"/>
+                  <Box sx={{borderLeft: 4, borderColor: '#eee', pl: 2}}>
+                    <MenuButton title="Log In" color='white'/>
+                    <MenuButton title="Create Account" color='black'/>
+                  </Box>
+                </Stack>
+          </Toolbar>
+        </AppBar>      
+      
+         
+
 
           <Stack direction='row' sx={{width: '100%', height: '100vh'}}>
+            
+         
             <Box sx={{borderRight: 4, borderColor: '#eee', height: '80%', width: '50%', display: "flex", justifyContent: 'center', alignItems: 'center'}}>
               <Box component='img' alt='phones' src={phones} width='400px' height='530px'></Box>
             </Box>
+         
             <Box sx={{height: '80%', display: 'flex', flexDirection: 'column', justifyContent: 'center', pl: '96px', pr: '256px' }}>
-              <Typography variant="h2" gutterBottom>Land the dream job by showing your soft skills credentials</Typography>
-              <Typography variant="h3"> Create your free talent wallet</Typography>
-              
+             
+
+     
+              <Box>
+                <Typography variant="h2" gutterBottom>Land the dream job by showing your soft skills credentials</Typography>
+                <Typography variant="h3"> Create your free talent wallet</Typography>
+              </Box>
+            
+
+
+
               <Stack spacing={2} sx={{mt: '36px', width:"60%"}} >
                 <TextField label="Display Name" type='text' onChange={(e) => setName(e.target.value)}></TextField>
                 <TextField label="Email address" type='email' onChange={(e) => setEmail(e.target.value)}></TextField>
@@ -59,18 +77,43 @@ const Desktop = () => {
                 ></Box>}
 
                 onClick={handleSignUpClick}
-                 >Sign up with email</Button>
+                 >Sign up with email</Button>               
                 
-                <Typography variant="body1">Already have an account? <Link to='#'>Log in</Link></Typography>
-
+                        
+                  <Typography variant="body1">Already have an account? <Link to='#'>Log in</Link></Typography>
+                
               </Stack>           
             </Box>
+      
           </Stack>
-
-
-
-
+  
       </Stack>
+}
+    
+
+{size.width <= 768 &&
+  <Stack width='100%' height='100vh' justifyContent='end' alignItems='center' spacing={2} paddingBottom={2}>
+    <Stack spacing={2} sx={{display: 'flex', flexDirection: 'column'}}>
+    <TextField label="Display Name" type='text' onChange={(e) => setName(e.target.value)} ></TextField>
+    <TextField label="Email address" type='email' onChange={(e) => setEmail(e.target.value)}  ></TextField>
+    <Button
+      variant="text"    
+      className='btn'
+        style={{ fontFamily: 'Roboto', fontSize: '28px', color: '#FFF' }}             
+        sx={{mr: 2, backgroundColor: '#222', borderRadius: '26px', height: '52px'}}
+        startIcon={<Box component='img' alt="magic link icon" src={magicLink} width='36px' height='36px'
+        ></Box>}
+
+        onClick={handleSignUpClick}
+        >Sign up with email</Button>  
+        </Stack>    
+
+  </Stack>
+
+
+
+}
+
 
     <Dialog open={open} sx={{m: 0, p: 0}}>
     <DialogContent sx={{width: '500px', height: '500px'}}>
